6b84f1347e7ae91d89592ccd607ea950aa0e1888
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-27 23:42:42 +0530

    added : found the cheapest hotel according to week day rates

--- main/java/model/Hotel.java
+++ main/java/model/Hotel.java
@@ -1,32 +1,55 @@
+/************************************************************
+ * Purpose POJO for Hotel Reservation System
+ * @author Rosy Rupali
+ * @Version 1.0
+ * @since 26-06-2021
+ *
+ ************************************************************/
 package model;
 
 public class Hotel {
 	private String hotelName;
 	private int weekDayRate;
-	private int weekendRate;
-
-
-	public Hotel(String hotelName, int weekDayRate, int weekendRate) {
+	private String typeOfCustomer;
+	private int costWeekDay;
+	
+	public Hotel(String hotelName, int weekDayRate) {
 		this.hotelName = hotelName;
 		this.weekDayRate = weekDayRate;
-		this.weekendRate = weekendRate;
 	}
+
 	public String getHotelName() {
 		return hotelName;
 	}
+
 	public void setHotelName(String hotelName) {
 		this.hotelName = hotelName;
 	}
+
 	public int getWeekDayRate() {
 		return weekDayRate;
 	}
+
 	public void setWeekDayRate(int weekDayRate) {
 		this.weekDayRate = weekDayRate;
 	}
-	public int getWeekendRate() {
-		return weekendRate;
+
+	public String getTypeOfCustomer() {
+		return typeOfCustomer;
 	}
-	public void setWeekendRate(int weekendRate) {
-		this.weekendRate = weekendRate;
+
+	public void setTypeOfCustomer(String typeOfCustomer) {
+		this.typeOfCustomer = typeOfCustomer;
 	}
+
+	public int getCostWeekDay() {
+		return costWeekDay;
+	}
+
+	public void setCostWeekDay(int costWeekDay) {
+		this.costWeekDay = costWeekDay;
+	}
+
+
+	
 }
