c6a4bab8fcdc1175984a193c0076fabeb407575a
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-27 14:47:14 +0530

    added: new hotel with name and rates for week day as well as weekend

---
+++ main/java/model/Hotel.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Hotel {
+	private String hotelName;
+	private int weekDayRate;
+	private int weekendRate;
+
+
+	public Hotel(String hotelName, int weekDayRate, int weekendRate) {
+		this.hotelName = hotelName;
+		this.weekDayRate = weekDayRate;
+		this.weekendRate = weekendRate;
+	}
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public int getWeekDayRate() {
+		return weekDayRate;
+	}
+	public void setWeekDayRate(int weekDayRate) {
+		this.weekDayRate = weekDayRate;
+	}
+	public int getWeekendRate() {
+		return weekendRate;
+	}
+	public void setWeekendRate(int weekendRate) {
+		this.weekendRate = weekendRate;
+	}
+}
