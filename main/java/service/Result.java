3d0d330267ecccd31a2e473a1019b80eef4c19fa
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-27 23:37:09 +0530

    added : found the cheapest hotel according o week day rates

---
+++ main/java/service/Result.java
@@ -0,0 +1,20 @@
+package service;
+
+public class Result {
+
+	String hotelName;
+	int totalCost;
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public int getTotalCost() {
+		return totalCost;
+	}
+	public void setTotalCost(int totalCost) {
+		this.totalCost = totalCost;
+	}
+	
+}
